fa5df905e50c2cd3758778929adfaa9eb14c4f4a
CodingWorker <dy10010@sina.cn>
2017-09-05 16:02:46 +0800

    add

--- php/composer/codes/www/index.php
+++ php/composer/codes/www/index.php
@@ -4,5 +4,6 @@ $str = require './third/vendor/autoload.php';
 $log = new Monolog\Logger('index');
 $log->pushHandler(new Monolog\Handler\StreamHandler('../log/app.log', Monolog\Logger::WARNING));
 
+
 $log->addWarning('Foo');
 
